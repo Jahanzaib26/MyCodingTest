0de874a9f28b195eb476f742d295aa650e89a7bf
Jahanzaib26 <m.jahanzaib1274@gmail.com>
2023-10-18 13:32:00 +0500

    CardFlipComplete
    Now that we have used bool to flip the cards.. it's time to start working on initiating other cards on the screen

--- Coding Test Jazzy/Assets/Scripts/MainCard.cs
+++ Coding Test Jazzy/Assets/Scripts/MainCard.cs
@@ -6,10 +6,12 @@ public class MainCard : MonoBehaviour
 {
     //         At first we're trying to Animate and Flip the card              //
     public GameObject Back_Card;
+    bool Allow_to_turn, front_Face;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Allow_to_turn = true;
+        front_Face = false;
     }
 
     // Update is called once per frame
@@ -20,38 +22,81 @@ public class MainCard : MonoBehaviour
 
     public void OnMouseDown()
     {
-        StartCoroutine(TurnFrontCard()); 
-    }
+        // StartCoroutine(TurnFrontCard()); 
+        if (Allow_to_turn) {
 
-
-    IEnumerator TurnFrontCard() {
-
-
-        // Fliping the cards by rotating to 180 degrees
-
-        for (float j = 0f; j <= 180; j += 10f) {
-
-            transform.rotation = Quaternion.Euler(0f, j, 0f);
-            yield return new WaitForSeconds(0.05f);
-        }
-
-        StartCoroutine(TurnBackCard());
-
-    }
-
-
-    IEnumerator TurnBackCard()
-    {
-        // Fliping the cards back to it's rotation
-
-        yield return new WaitForSeconds(0.5f);
-        for (float j = 180; j >= 0; j -= 10) {
-
-            transform.rotation = Quaternion.Euler(0f, j, 0f);
-
-            yield return new WaitForSeconds(0.05f);
-
+            StartCoroutine(TurnTheCards());
+        
         }
 
     }
+
+
+    IEnumerator TurnTheCards() {
+        Allow_to_turn = false;
+        if (!front_Face)
+        {
+
+            for (float j = 0f; j <= 180; j += 10f)
+            {
+
+                transform.rotation = Quaternion.Euler(0f, j, 0f);
+                yield return new WaitForSeconds(0.05f);
+            }
+
+
+        }
+
+
+        else if (front_Face)
+        {
+
+            for (float j = 180; j >= 0; j -= 10)
+            {
+
+                transform.rotation = Quaternion.Euler(0f, j, 0f);
+
+                yield return new WaitForSeconds(0.05f);
+
+            }
+        }
+
+
+
+        Allow_to_turn = true;
+        front_Face = !front_Face;
+
+        }
     }
+
+    //IEnumerator TurnFrontCard() {
+
+
+    //    // Fliping the cards by rotating to 180 degrees
+
+    //    for (float j = 0f; j <= 180; j += 10f) {
+
+    //        transform.rotation = Quaternion.Euler(0f, j, 0f);
+    //        yield return new WaitForSeconds(0.05f);
+    //    }
+
+    //    StartCoroutine(TurnBackCard());
+
+    //}
+
+
+    //IEnumerator TurnBackCard()
+    //{
+    //    // Fliping the cards back to it's rotation
+
+    //    yield return new WaitForSeconds(0.5f);
+    //    for (float j = 180; j >= 0; j -= 10) {
+
+    //        transform.rotation = Quaternion.Euler(0f, j, 0f);
+
+    //        yield return new WaitForSeconds(0.05f);
+
+    //    }
+
+    //}
+    

f07e6d525cbcf1f58e7cc21701ec8988fb8df65d
Jahanzaib26 <m.jahanzaib1274@gmail.com>
2023-10-18 12:58:22 +0500

    Flipping Card
    First i'm rotating the card .. i was using simple activeself function first to just show the main card and hide the back card but it wasn't giving a good vibe so i decided to flip the card using rotation

---
+++ Coding Test Jazzy/Assets/Scripts/MainCard.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour
+{
+    //         At first we're trying to Animate and Flip the card              //
+    public GameObject Back_Card;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        StartCoroutine(TurnFrontCard()); 
+    }
+
+
+    IEnumerator TurnFrontCard() {
+
+
+        // Fliping the cards by rotating to 180 degrees
+
+        for (float j = 0f; j <= 180; j += 10f) {
+
+            transform.rotation = Quaternion.Euler(0f, j, 0f);
+            yield return new WaitForSeconds(0.05f);
+        }
+    
+    }
+}

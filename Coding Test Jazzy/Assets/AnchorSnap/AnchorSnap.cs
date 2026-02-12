7eca4ec8423d4b45fedf2c199b8d7b19cf53c8e8
anassaeed123gma <anassaeed123gma@gmail.com>
2026-02-12 13:20:39 +0500

    settings pannel

---
+++ Coding Test Jazzy/Assets/AnchorSnap/AnchorSnap.cs
@@ -0,0 +1,96 @@
+﻿//Poylgon Planet - Contact. https://polygonplanet.com/contact/
+//Copyright © 2016-2018 Polygon Planet. All rights reserved. https://polygonplanet.com/privacy-policy/
+//This source file is subject to Unity Technologies Asset Store Terms of Service. https://unity3d.com/legal/as_terms
+#pragma warning disable 0168 //Variable declared, but not used.
+#pragma warning disable 0219 //Variable assigned, but not used.
+#pragma warning disable 0414 //Private field assigned, but not used.
+#pragma warning disable 0649 //Variable asisgned to, and will always have default value.
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+public class AnchorSnapEditor : Editor
+{
+[MenuItem("Anchor Snap/parent and children", false, 0)]
+private static void SnapParentAndChildrenMenuItem()
+{
+SnapAnchorsMultiple(Selection.activeGameObject);
+}
+[MenuItem("Anchor Snap/just parent", false, 0)]
+private static void SnapParentMenuItem()
+{
+SnapAnchors(Selection.activeGameObject);
+}
+[MenuItem("GameObject/Anchor Snap/parent and children %[", false, 0)]
+private static void SnapParentAndChildrenGameObject()
+{
+SnapAnchorsMultiple(Selection.activeGameObject);
+}
+[MenuItem("GameObject/Anchor Snap/just parent %]", false, 0)]
+private static void SnapParentGameObject()
+{
+SnapAnchors(Selection.activeGameObject);
+}
+private static void SnapAnchors(GameObject gameObject)
+{
+RectTransform recTransform = null;
+RectTransform parentTransform = null;
+if (gameObject.transform.parent != null)
+{
+if (gameObject.GetComponent<RectTransform>() != null)
+{
+recTransform = gameObject.GetComponent<RectTransform>();
+}
+else
+{
+return;
+}
+if (parentTransform == null)
+{
+parentTransform = gameObject.transform.parent.GetComponent<RectTransform>();
+}
+Undo.RecordObject(recTransform, "Snap Anchors");
+//Vector2 offsetMin = recTransform.offsetMin;
+//Vector2 offsetMax = recTransform.offsetMax;
+//Vector2 anchorMin = recTransform.anchorMin;
+//Vector2 anchorMax = recTransform.anchorMax;
+
+//Debug.Log(offsetMin + " " + offsetMax);
+//Vector2 parent_scale = new Vector2(parentTransform.rect.width, parentTransform.rect.height);
+
+//recTransform.anchorMin = new Vector2(anchorMin.x + (offsetMin.x / parent_scale.x), anchorMin.y + (offsetMin.y / parent_scale.y));
+//recTransform.anchorMax = new Vector2(anchorMax.x + (offsetMax.x / parent_scale.x), anchorMax.y + (offsetMax.y / parent_scale.y));
+//recTransform.offsetMin = Vector2.zero;
+//recTransform.offsetMax = Vector2.zero;
+
+
+Vector2 pivot = recTransform.pivot;
+Vector2 parentSize = parentTransform.rect.size;
+// Calculate the new anchor values based on the pivot
+Vector2 newAnchorMin = new Vector2(
+recTransform.anchorMin.x + (recTransform.anchoredPosition.x / parentSize.x),
+recTransform.anchorMin.y + (recTransform.anchoredPosition.y / parentSize.y)
+);
+Vector2 newAnchorMax = new Vector2(
+recTransform.anchorMax.x + (recTransform.anchoredPosition.x / parentSize.x),
+recTransform.anchorMax.y + (recTransform.anchoredPosition.y / parentSize.y)
+);
+// Set the anchors to the pivot value
+recTransform.anchorMin = newAnchorMin;
+recTransform.anchorMax = newAnchorMax;
+// Reset the anchored position to zero
+recTransform.anchoredPosition = Vector2.zero;
+//recTransform.offsetMin = Vector2.zero;
+//recTransform.offsetMax = Vector2.zero;
+
+}
+}
+private static void SnapAnchorsMultiple(GameObject gameObject)
+{
+SnapAnchors(gameObject);
+for (int i = 0; i < gameObject.transform.childCount; i++)
+{
+SnapAnchorsMultiple(gameObject.transform.GetChild(i).gameObject);
+}
+}
+}
+#endif
--- Coding Test Jazzy/Assets/Scripts/MainMenuGameManager.cs
+++ Coding Test Jazzy/Assets/Scripts/MainMenuGameManager.cs
@@ -5,15 +5,55 @@ using UnityEngine;
 
 public class MainMenuGameManager : MonoBehaviour
 {
+
+    private int[] qualityLevels = { 1, 3, 5 };
+    // 0 = Low
+    // 1 = Medium
+    // 2 = Ultra
+
     public GameObject MainmenuPannel;
     public GameObject privacypannel;
     public GameObject Exitpannel;
+    public GameObject settingspannel;
+    public GameObject soundon;
+    public GameObject soundof;
+    public GameObject musicon;
+    public GameObject musicof;
+
+    [Header("Audio Sources")]
+    public AudioSource[] soundSources;   // SFX ke liye
+    public AudioSource[] musicSources;   // Music ke liye
+
+    public GameObject low;
+    public GameObject medium;
+    public GameObject high;
+
+    private int currentIndex = 2; // Default Ultra
+
     // Start is called before the first frame update
     void Start()
     {
+        ApplyQuality();
         MainmenuPannel.SetActive(true);
         privacypannel.SetActive(false);
         Exitpannel.SetActive(false);
+        soundon.SetActive(true);
+        musicon.SetActive(true);
+        low.SetActive(false);
+        medium.SetActive(false);
+        high.SetActive(true);
+        settingspannel.SetActive(false);
+
+    }
+
+    void ApplyQuality()
+    {
+        int unityIndex = qualityLevels[currentIndex];
+        QualitySettings.SetQualityLevel(unityIndex);
+
+        low.SetActive(currentIndex == 0);
+        medium.SetActive(currentIndex == 1);
+        high.SetActive(currentIndex == 2);
     }
 
     // Update is called once per frame
@@ -25,6 +65,10 @@ public class MainMenuGameManager : MonoBehaviour
     public void Exit()
     {
         Exitpannel.SetActive(true);
+    }
+    public void Settings()
+    {
+        settingspannel.SetActive(true);
     }
 
     public void privacy()
@@ -47,6 +91,76 @@ public class MainMenuGameManager : MonoBehaviour
     public void no()
     {
         Exitpannel.SetActive(false);
+    }
+
+    public void SoundON()
+    {
+        soundon.SetActive(false);
+        soundof.SetActive(true);
+
+        foreach (AudioSource source in soundSources)
+        {
+            source.mute = true;   // sound OFF
+        }
+    }
+
+    public void SoundOF()
+    {
+        soundon.SetActive(true);
+        soundof.SetActive(false);
+
+        foreach (AudioSource source in soundSources)
+        {
+            source.mute = false;   // sound ON
+        }
+    }
+
+    public void MusicON()
+    {
+        musicon.SetActive(false);
+        musicof.SetActive(true);
+
+        foreach (AudioSource source in musicSources)
+        {
+            source.mute = true;   // music OFF
+        }
+    }
+
+    public void MusicOF()
+    {
+        musicon.SetActive(true);
+        musicof.SetActive(false);
+
+        foreach (AudioSource source in musicSources)
+        {
+            source.mute = false;   // music ON
+        }
+    }
+
+    public void leftarrow()
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+            ApplyQuality();
+        }
+    }
+
+
+
+    public void Rightarrow()
+    {
+        if (currentIndex < qualityLevels.Length - 1)
+        {
+            currentIndex++;
+            ApplyQuality();
+        }
+    }
+
+
+    public void save()
+    {
+        settingspannel.SetActive(false);
     }
 
 }
